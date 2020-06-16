f9b53eed8307736af4bb358d432f61f36d92d673
abdiasviana <abdviana@gmail.com>
2020-06-15 22:01:01 -0300

    Finalizando a primeira parte do projeto

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   #get "produtos", to: "produtos#index"
-  get "produtos/new", to: "produtos#new"
-  post "produtos", to: "produtos#create"
-  delete "produtos/:id", to: "produtos#destroy", as: :produto
+  #get "produtos/new", to: "produtos#new"
+  #post "produtos", to: "produtos#create"
+  #delete "produtos/:id", to: "produtos#destroy", as: :produto
+
+
+  get 'produtos/busca', to: "produtos#busca", as: :busca_produto
+
+  resources :produtos, only: [:new, :create, :destroy]
 
   root to: "produtos#index"
   
